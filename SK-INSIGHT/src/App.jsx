ece20c295399adbdd944f6c9ec6018ed4a8392c0
neil761 <alvarezneilandrew486@gmail.com>
2025-05-21 16:00:58 +0800

    admin

--- SK-INSIGHT/src/App.jsx
+++ SK-INSIGHT/src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import AdminLogin from './pages/adminLogin';
+import Dashboard from './pages/dashboard';
+import KKProfile from './pages/kkProfile';
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
+
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/kk-profile" element={<KKProfile />} />
       </Routes>
     </Router>
   );
